dd2aa65f6ec297e6c9c459da6e69fa4b4568c945
Steve Potter <stephen@redefinesurgery.com>
2024-05-01 13:27:46 -0400

    Got it working with ApplicationLoadBalancedEc2Service

--- cdk/src/ecs-stack.ts
+++ cdk/src/ecs-stack.ts
@@ -15,6 +15,7 @@ import { ManagedPolicy, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam'
 import { Construct } from 'constructs'
 import { ApplicationLoadBalancer, ApplicationProtocol } from 'aws-cdk-lib/aws-elasticloadbalancingv2'
 import { AutoScalingGroup, GroupMetrics, HealthCheck } from 'aws-cdk-lib/aws-autoscaling'
+import { ApplicationLoadBalancedEc2Service } from 'aws-cdk-lib/aws-ecs-patterns'
 
 export class EcsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -30,79 +31,28 @@ export class EcsStack extends Stack {
     const cluster = new Cluster(this, 'EcsCluster', {
       clusterName: id,
       vpc,
+      capacity: {
+        instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MEDIUM),
+      }
     })
 
-    const autoScalingGroup = new AutoScalingGroup(this, 'AutoScalingGroup', {
-      autoScalingGroupName: id,
-      vpc,
-      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.SMALL),
-      machineImage: EcsOptimizedImage.amazonLinux2023(AmiHardwareType.STANDARD),
-    })
-    cluster.addAsgCapacityProvider(new AsgCapacityProvider(this, 'AsgCapacityProvider', { autoScalingGroup }))
-
-    const logging = new AwsLogDriver({ streamPrefix: id }) 
-
-    // Create an IAM Role for ECS tasks
-    const ecsTaskExecutionRole = new Role(this, 'TaskExecutionRole', {
-      roleName: `${id}-container`,
-      assumedBy: new ServicePrincipal('ecs-tasks.amazonaws.com'),
-      description: 'Role for the ECS container.',
-    })
-    // Attach policies to the role
-    ecsTaskExecutionRole.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonECSTaskExecutionRolePolicy'))
-
-    // Define the ECS Task Definition
-    const taskDefinition = new Ec2TaskDefinition(this, 'TaskDef', {
-      executionRole: ecsTaskExecutionRole,
-      family: id,
-    })
-
-    const containerPort = 5100
-    const containerName = 'web'
-    const container = taskDefinition.addContainer('AppContainer', {
-      containerName,
-      image: ContainerImage.fromRegistry("stevepotterredefine/simple_server:latest"),      
-      cpu: 256, // 0.25 vCPU
-      memoryLimitMiB: 512,
-      logging,
-    })
-
-    container.addPortMappings({
-      containerPort,
-      protocol: Protocol.TCP,
-    })
-
-    // Define the ECS Service
-    const service = new Ec2Service(this, 'EcsService', {
+    // source: https://github.com/aws/aws-cdk/blob/c389a8be79297239f76d680301aad3aa135334d6/packages/aws-cdk-lib/aws-ecs-patterns/lib/ecs/application-load-balanced-ecs-service.ts
+    // base class source: https://github.com/aws/aws-cdk/blob/c389a8be79297239f76d680301aad3aa135334d6/packages/aws-cdk-lib/aws-ecs-patterns/lib/base/application-load-balanced-service-base.ts
+    const s = new ApplicationLoadBalancedEc2Service(this, 'Service', {
       cluster,
-      taskDefinition,
+      memoryLimitMiB: 512,
       serviceName: id,
+      cpu: 256,
+      taskImageOptions: {
+        image: ContainerImage.fromRegistry("stevepotterredefine/simple_server:latest"),
+        containerPort: 5100,
+        enableLogging: true,
+        logDriver: AwsLogDriver.awsLogs({ streamPrefix: id }),
+      },
+      publicLoadBalancer: true,
     })
+    s.taskDefinition.
 
-    // Define the ALB
-    const alb = new ApplicationLoadBalancer(this, 'LoadBalancer', {
-      vpc,
-      internetFacing: true,
-      deletionProtection: false, // so it can be easily deleted
-      loadBalancerName: id,
-    })
-
-    // Attach the ALB to the ECS Service
-    const listener = alb.addListener('PublicListener', {
-      port: 80,
-      open: true,
-    })
-    listener.addTargets('ECS', {
-      protocol: ApplicationProtocol.HTTP,
-      targetGroupName: id,
-      targets: [
-        service.loadBalancerTarget({
-          containerName,
-          containerPort,
-        }),
-      ],
-    })
-
-    new CfnOutput(this, `${id}-DNS`, { value: alb.loadBalancerDnsName })
+    new CfnOutput(this, `${id}-DNS`, { value: s.loadBalancer.loadBalancerDnsName })
   }
 }
